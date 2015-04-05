51766b972e7813d1e7d44ae22a692e2e4a768452
Nishant Gill <gillnishant@tamu.edu>
2015-04-04 23:32:24 -0500

    Update PriorityCache.java

--- src/CacheImprovement/PriorityCache.java
+++ src/CacheImprovement/PriorityCache.java
@@ -1,7 +1,7 @@
 package CacheImprovement;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class PriorityCache {
 	double hitRatio;
@@ -11,14 +11,18 @@ public class PriorityCache {
 	public final double alpha=0.8;
 	public final double beta=0.2;
 	
-	PriorityQueue<PriorityCacheNode> pcache;
+	PriorityQueue<String> pcache;
+	HashMap<String,PriorityCacheNode> hm;
 	public PriorityCache(int s){
 		this.maxSize = s;
 		this.hitRatio =0;
 		this.missRatio=0;
-		this.total=0;		
-		this.pcache = new PriorityQueue<PriorityCacheNode>(maxSize, new Comparator<PriorityCacheNode>(){
-			public int compare(PriorityCacheNode n1, PriorityCacheNode n2){
+		this.total=0;	
+		this.hm = new HashMap<String,PriorityCacheNode>();
+		this.pcache = new PriorityQueue<String>(maxSize, new Comparator<String>(){
+			public int compare(String s1, String s2){
+				PriorityCacheNode n1=hm.get(s1);
+				PriorityCacheNode n2=hm.get(s2);				
 				if (n1.priority < n2.priority)
 					return -1;
 				if (n1.priority > n2.priority)
@@ -27,34 +31,36 @@ public class PriorityCache {
 			}
 		});
 	}
-	private PriorityCacheNode findNodeByKey(Object userKey) {
-		for(PriorityCacheNode n : this.pcache){
-			if(n.key.equals((String)userKey))
-				return n;
-		}
-		return null;
-	}	
+	
 	
 	public void shrinkToSize(int s){
-		if(this.pcache.size() == s)
-			this.pcache.remove();
+		if(this.pcache.size() == s){
+			String key = this.pcache.poll();		
+			hm.remove(key);
+		}
 	}
 	
 	public void addObject(String userKey) {//change to object later
-		PriorityCacheNode node;
+		PriorityCacheNode node = null;
 		this.total += 1;
-		node = findNodeByKey(userKey);
+		if(this.hm.containsKey(userKey)){
+			node = this.hm.get(userKey);
+		}
+		
+		//node = findNodeByKey(userKey);
 		if (node != null) {
 			this.hitRatio += 1;
-			this.pcache.remove(node);
 			node.setValue(alpha,beta);
-			this.pcache.add(node);
+			this.hm.put(userKey, node);
+			this.pcache.remove(userKey);			
+			this.pcache.add(userKey);			
 		} else {
 			this.missRatio += 1;
 			double new_priority=alpha+beta*10;
-			node = new PriorityCacheNode(userKey,1,System.currentTimeMillis(),new_priority);
+			node = new PriorityCacheNode(1,System.currentTimeMillis(),new_priority);
 			shrinkToSize(maxSize);
-			this.pcache.add(node);
+			this.hm.put(userKey, node);
+			this.pcache.add(userKey);
 		}
 	}
 
